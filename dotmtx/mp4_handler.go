d965790d7c33385d72ac3fb5cfeba095770b1636
Fabio Massaioli <fabio.massaioli@gmail.com>
2022-05-01 17:36:24 +0200

    Convert gifs as temporary files

--- dotmtx/mp4_handler.go
+++ dotmtx/mp4_handler.go
@@ -2,9 +2,11 @@ package dotmtx
 
 import (
 	"image/gif"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 
 	"petbots.fbbdev.it/dotmtxbot/log"
@@ -49,54 +51,55 @@ func Mp4Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	tmpDir, err := ioutil.TempDir("", "dotmtxbot_mp4")
+	if err != nil {
+		log.ErrorLogger.Print("ioutil: ", err)
+		log.WarningLogger.Print("temporary dir creation failed")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer os.RemoveAll(tmpDir)
+
+	gifPath := filepath.Join(tmpDir, "dotmtx.gif")
+	mp4Path := filepath.Join(tmpDir, "dotmtx.mp4")
+
+	gifFile, err := os.Create(gifPath)
+	if err != nil {
+		log.ErrorLogger.Print("os: ", err)
+		log.WarningLogger.Print("temporary file creation failed")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = gif.EncodeAll(gifFile, anim)
+	gifFile.Close()
+	if err != nil {
+		log.ErrorLogger.Print("gif: ", err)
+		log.WarningLogger.Print("could not write GIF to temporary file")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
 	ffmpeg := exec.CommandContext(
 		r.Context(),
 		"ffmpeg",
-		// "-hide_banner",
-		// "-loglevel", "error",
-		"-f", "gif",
-		"-i", "pipe:",
-		"-movflags", "frag_keyframe+empty_moov",
+		"-hide_banner",
+		"-loglevel", "error",
+		"-i", gifPath,
+		"-movflags", "+faststart",
 		"-pix_fmt", "yuv420p",
-		"-f", "mp4",
-		"pipe:",
+		mp4Path,
 	)
 
-	ffmpegInput, err := ffmpeg.StdinPipe()
-	if err != nil {
-		log.ErrorLogger.Print("exec: ", err)
-		log.WarningLogger.Print("ffmpeg input pipe creation failed")
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	ffmpeg.Stdout = w
 	ffmpeg.Stderr = os.Stderr
 
-	err = ffmpeg.Start()
-	if err != nil {
-		log.ErrorLogger.Print("exec: ", err)
-		log.WarningLogger.Print("ffmpeg startup failed")
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	go func() {
-		defer ffmpegInput.Close()
-		if err := gif.EncodeAll(ffmpegInput, anim); err != nil {
-			log.ErrorLogger.Print("gif: ", err)
-			log.WarningLogger.Print("gif encoding and transfer to ffmpeg failed")
-			ffmpeg.Process.Kill()
-		}
-	}()
-
-	w.Header().Add("Content-Type", "video/mp4")
-	err = ffmpeg.Wait()
+	err = ffmpeg.Run()
 	if err != nil {
 		log.ErrorLogger.Print("exec: ", err)
 		log.WarningLogger.Print("gif to mp4 conversion failed")
-		// just in case nothing was written
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+
+	http.ServeFile(w, r, mp4Path)
 }
