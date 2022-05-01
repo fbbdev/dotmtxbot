e80874d57796ce90e46d1a6e0ca367e7c337edff
Fabio Massaioli <fabio.massaioli@gmail.com>
2022-05-01 12:55:12 +0200

    Add setup script

---
+++ config/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "updating dependencies"
+go get -u
+go mod tidy
+
+echo "building bot binary"
+go build
+
+echo "installing bot"
+mkdir -p "$HOME/bin"
+mv "dotmtxbot" "$HOME/bin/dotmtxbot"
+
+echo "installing systemd configuration"
+mkdir -p "$HOME/.config/systemd/user"
+cp -a "config/dotmtxbot.service" "$HOME/.config/systemd/user"
+
+echo "enabling and restarting systemd service"
+systemctl --user enable dotmtxbot.service
+systemctl --user restart dotmtxbot.service
